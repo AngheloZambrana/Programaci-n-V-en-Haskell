89f871f8fc89192bc006c71c29d57e7e50c1167f
AngheloZambrana <anghelozambrana2005@gmail.com>
2024-04-18 10:10:58 -0400

    Apuntes del dia de hoy

--- Codigos/11abril.hs
+++ Codigos/11abril.hs
@@ -5,6 +5,7 @@ import Control.Applicative
 data CMaybe a = CNothing | CJust Int a deriving(Show)
 
 instance Functor CMaybe where
+    fmap :: (a -> b) -> CMaybe a -> CMaybe b
     fmap f CNothing = CNothing
     fmap f (CJust counter x) = CJust (counter + 1) (f x)
 
---
+++ Codigos/18abril.hs
@@ -0,0 +1,23 @@
+module Codigos where
+import Control.Applicative
+
+--instance Applicative [] where
+    -- pure :: a -> [a]
+   -- pure x = [x]
+    -- (<*>) :: [a -> b] -> [a] -> [b]
+   -- gs <*> xs = [g x | g <- gs, x <- xs]
+
+zipList1 :: ZipList Int
+zipList1 = ZipList [1,2,3]
+
+zipList2 :: ZipList Int
+zipList2 = ZipList [4,5,6]
+
+result :: ZipList Int
+result = (*) <$> zipList1 <*> zipList2
+
+getChar2 :: Int -> IO String
+getChar2 0 = return []
+getChar2 n = pure () <*> getChar <*> getChar2 (n - 1)
+
+getChar :: IO Char
